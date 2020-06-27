6c57670080b8b528c4fed31a2c50618e076fd2be
Nguyen Loan <loanloannguyen0038@gmail.com>
2020-06-27 10:25:41 +0700

    quan tri

---
+++ WebBanGiay_226/WebBanGiay_226/Areas/Admin/Controllers/ChiTietSanPhamsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebBanGiay_226.Models.EF;
+
+namespace WebBanGiay_226.Areas.Admin.Controllers
+{
+    public class ChiTietSanPhamsController : Controller
+    {
+        private WebGiay db = new WebGiay();
+
+        // GET: Admin/ChiTietSanPhams
+        public ActionResult Index()
+        {
+            var chiTietSanPhams = db.ChiTietSanPhams.Include(c => c.SanPham);
+            return View(chiTietSanPhams.ToList());
+        }
+
+        // GET: Admin/ChiTietSanPhams/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChiTietSanPham chiTietSanPham = db.ChiTietSanPhams.Find(id);
+            if (chiTietSanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chiTietSanPham);
+        }
+
+        // GET: Admin/ChiTietSanPhams/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaSanPham = new SelectList(db.SanPhams, "MaSanPham", "TenSanPham");
+            return View();
+        }
+
+        // POST: Admin/ChiTietSanPhams/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaCTSP,MetaTitle,Code,Mau,Size,SoLuong,LinkAnh,TrangThai,MaSanPham")] ChiTietSanPham chiTietSanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ChiTietSanPhams.Add(chiTietSanPham);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaSanPham = new SelectList(db.SanPhams, "MaSanPham", "TenSanPham", chiTietSanPham.MaSanPham);
+            return View(chiTietSanPham);
+        }
+
+        // GET: Admin/ChiTietSanPhams/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChiTietSanPham chiTietSanPham = db.ChiTietSanPhams.Find(id);
+            if (chiTietSanPham == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaSanPham = new SelectList(db.SanPhams, "MaSanPham", "TenSanPham", chiTietSanPham.MaSanPham);
+            return View(chiTietSanPham);
+        }
+
+        // POST: Admin/ChiTietSanPhams/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaCTSP,MetaTitle,Code,Mau,Size,SoLuong,LinkAnh,TrangThai,MaSanPham")] ChiTietSanPham chiTietSanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(chiTietSanPham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaSanPham = new SelectList(db.SanPhams, "MaSanPham", "TenSanPham", chiTietSanPham.MaSanPham);
+            return View(chiTietSanPham);
+        }
+
+        // GET: Admin/ChiTietSanPhams/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChiTietSanPham chiTietSanPham = db.ChiTietSanPhams.Find(id);
+            if (chiTietSanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chiTietSanPham);
+        }
+
+        // POST: Admin/ChiTietSanPhams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            ChiTietSanPham chiTietSanPham = db.ChiTietSanPhams.Find(id);
+            db.ChiTietSanPhams.Remove(chiTietSanPham);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebBanGiay_226/WebBanGiay_226/Areas/Admin/Controllers/PhanHoisController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebBanGiay_226.Models.EF;
+
+namespace WebBanGiay_226.Areas.Admin.Controllers
+{
+    public class PhanHoisController : Controller
+    {
+        private WebGiay db = new WebGiay();
+
+        // GET: Admin/PhanHois
+        public ActionResult Index()
+        {
+            var phanHois = db.PhanHois.Include(p => p.NguoiDung);
+            return View(phanHois.ToList());
+        }
+
+        // GET: Admin/PhanHois/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhanHoi phanHoi = db.PhanHois.Find(id);
+            if (phanHoi == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phanHoi);
+        }
+
+        // GET: Admin/PhanHois/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaNguoiDung = new SelectList(db.NguoiDungs, "MaNguoiDung", "TenNguoiDung");
+            return View();
+        }
+
+        // POST: Admin/PhanHois/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaPhanHoi,MaNguoiDung,NoiDung,NgayPhanHoi")] PhanHoi phanHoi)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PhanHois.Add(phanHoi);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaNguoiDung = new SelectList(db.NguoiDungs, "MaNguoiDung", "TenNguoiDung", phanHoi.MaNguoiDung);
+            return View(phanHoi);
+        }
+
+        // GET: Admin/PhanHois/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhanHoi phanHoi = db.PhanHois.Find(id);
+            if (phanHoi == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaNguoiDung = new SelectList(db.NguoiDungs, "MaNguoiDung", "TenNguoiDung", phanHoi.MaNguoiDung);
+            return View(phanHoi);
+        }
+
+        // POST: Admin/PhanHois/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaPhanHoi,MaNguoiDung,NoiDung,NgayPhanHoi")] PhanHoi phanHoi)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(phanHoi).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaNguoiDung = new SelectList(db.NguoiDungs, "MaNguoiDung", "TenNguoiDung", phanHoi.MaNguoiDung);
+            return View(phanHoi);
+        }
+
+        // GET: Admin/PhanHois/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhanHoi phanHoi = db.PhanHois.Find(id);
+            if (phanHoi == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phanHoi);
+        }
+
+        // POST: Admin/PhanHois/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PhanHoi phanHoi = db.PhanHois.Find(id);
+            db.PhanHois.Remove(phanHoi);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
